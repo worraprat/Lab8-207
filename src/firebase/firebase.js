a8d295054b12c3270624a7ce2665d56b1f2799c1
worraprat <46592531+worraprat@users.noreply.github.com>
2020-03-25 18:32:28 +0700

    Add files via upload

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+import config from './config'
+
+console.log(config)
+
+if (!firebase.apps.length) {
+   firebase.initializeApp(config)
+}
+ 
+export default firebase
